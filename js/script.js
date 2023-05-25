900c05c4289144d304aa7205b8e58c9c66c7723a
Kalynn Powell <128936392+kmarie0420@users.noreply.github.com>
2023-05-25 15:57:20 -0500

    added comments to js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// all variables throughout page
+
 var searchBtn = document.querySelector("#search-btn");
 var searchBar = document.querySelector("#search-bar");
 var temperature = document.querySelector("#temperature");
@@ -11,15 +13,18 @@ var futureTemp = document.querySelectorAll(".future-temp")
 var futureDate = document.querySelectorAll(".future-date")
 var futureImg = document.querySelectorAll("#futureimg")
 var futureHumidity = document.querySelectorAll(".future-humidity")
+// openweathermap api
 var apiKey = "dd935dc2c8ab3520b53d9389d0e160b9";
 
-
+// fetchs current weather and provides current weather conditions
 function getWeatherData(cityName) {
+  // had to add units=imperial to get the corrects metric system
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
   return fetch(url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data)
+      // calling for icons specifically from openweather map api
       currentImg.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
        temperature.textContent = `${Math.floor(data.main.temp)}°F`;
        condition.textContent = data.weather[0].main;
@@ -28,14 +33,14 @@ function getWeatherData(cityName) {
        windspeed.textContent = data.wind.speed;
     });
 }
-
+// click event for the search button
 searchBtn.addEventListener("click", () => {
   var cityName = searchBar.value;
   console.log(cityName)
   getWeatherData(cityName);
   getForecastData(cityName)
 });
-
+// future forecast function. fetches future weather conditions for searched city
 function getForecastData(cityName) {
   url =
    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`;
@@ -44,6 +49,7 @@ function getForecastData(cityName) {
     .then((data) => {
       for(let i = 0; i < futureCard.length; i++) {
         const index = i * 8 + 4;
+        // calling for icons from openweathermap api
         futureImg[i].setAttribute("src", `https://openweathermap.org/img/wn/${data.list[index].weather[0].icon}@2x.png`)
         futureTemp[i].innerHTML = `${Math.floor(data.list[index].main.temp)}°F`
         
@@ -54,4 +60,4 @@ function getForecastData(cityName) {
     });
 }
 
-// // // URL is https://openweathermap.org/img/wn/10d@2x.png ----> icon url for openweather map
+// // // URL is https://openweathermap.org/img/wn/10d@2x.png ----> icons url for openweather map
